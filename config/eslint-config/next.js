665e54f684bd4ba37ac4429fc6d940cdc3a088cc
Gustavo-de-Almeida <gustavodealmeida01@gmail.com>
2025-10-01 18:35:20 -0300

    Project initial setup

---
+++ config/eslint-config/next.js
@@ -0,0 +1,8 @@
+/** @typedef {import('eslint').Linter.Config} */
+module.exports = {
+  extends: ['@rocketseat/eslint-config/next'],
+  plugins: ['simple-import-sort'],
+  rules: {
+    'simple-import-sort/imports': 'error'
+  }
+}
---
+++ config/eslint-config/node.js
@@ -0,0 +1,8 @@
+/** @typedef {import('eslint').Linter.Config} */
+module.exports = {
+  extends: ['@rocketseat/eslint-config/node'],
+  plugins: ['simple-import-sort'],
+  rules: {
+        'simple-import-sort/imports': 'error'
+  }
+}
